0c3ec22080f21ee83d3509f229d2764e7fcf9a99
Jolly jester ⭐ <113088298+Somveer-Duhan@users.noreply.github.com>
2024-07-13 16:26:31 +0530

    Implemented Routes

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Dashboard=()=>{
+    return(
+        <div>
+            <Navbar/>
+            <h1>Dashboard Page</h1>
+        </div>
+    );
+};
+export default Dashboard;
---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Signup=()=>{
+    return (
+        <div>
+            <Navbar/>
+            <h1>SignUp Page</h1>
+        </div>
+    );
+};
+export default Signup;
