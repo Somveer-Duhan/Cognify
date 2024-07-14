ce7a0167e54b158e89cd7d4467b44181b8f006ec
Jolly jester ⭐ <113088298+Somveer-Duhan@users.noreply.github.com>
2024-07-14 15:23:47 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Route,Routes,Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Loginpage from './pages/Loginpage';
+import Signup from './pages/Signup';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router basename="/Somveer-Duhan/cognify.git">
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard"/>}/>
+        <Route path ="/login" element={<Loginpage/>}/>
+        <Route path ="/signup" element={<Signup/>}/>
+        <Route path ="/dashboard" element={<ProtectedRoute><Dashboard></Dashboard></ProtectedRoute>}/>
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
+// The ProtectedRoute component ensures that only authenticated users 
+// can access certain routes in your application. If a user tries to access
+// a protected route without being authenticated, they are redirected to the login page.
+
