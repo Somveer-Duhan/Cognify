9903477abbff90a760ecd323c04c9838fff31f74
Jolly jester ⭐ <113088298+Somveer-Duhan@users.noreply.github.com>
2024-07-14 15:19:49 +0530

    implemented Protected Routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Navbar=()=>{
-    return(
+const Navbar = () => {
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">Cognify</Link>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Dashboard</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/signup">Sign Up</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-    );
+      <Link className="navbar-brand" to="/">Cognify</Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Dashboard</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
+
 export default Navbar;
